b8e73613b4e6e8c370d92be5ccae52f7b32c5797
Gabriel Andres Toro Cerda <gabriel.toro@usach.cl>
2023-08-14 04:05:40 -0400

    feat: filter by post name now it is available

--- src/Views/PostManagement/PostManagement.js
+++ src/Views/PostManagement/PostManagement.js
@@ -26,6 +26,7 @@ const PostManagement = () => {
         axios.get(POST_INDEX)
         .then(response => {
             setPosts(response.data);
+            setFilteredPosts(response.data);
         })
         .catch(error => console.log(error));
     }
@@ -35,12 +36,14 @@ const PostManagement = () => {
     },[])
 
     // Hooks
-    const [posts, setPosts] = useState([])
+    const [posts, setPosts] = useState([]);
+    const [filteredPost, setFilteredPosts] = useState([]);
+    const [filterValue, setFilterValue] = useState('');
     const [formulario, handleChange, reset] = useFormulario({ 
         id: -1,
         post: '', 
         description: '',
-    })
+    });
     
     // Crud
     const addPost = (e) => {
@@ -53,7 +56,11 @@ const PostManagement = () => {
             setPosts([
                 ...posts,
                 response.data,
-              ])
+            ])
+            setFilteredPosts([
+                ...filteredPost,
+                response.data,
+            ])
             console.log(posts);
         })
         .catch(error => console.log(error));
@@ -65,9 +72,15 @@ const PostManagement = () => {
         .then(response => {
             const newPosts = posts.filter( postData => postData.id !== response.data.id);
             setPosts(newPosts);
+            setFilteredPosts(newPosts);
             console.log(posts);
         })
         .catch(error => console.log(error));
+    }
+
+    // filtro por nombre
+    const applyFilter = (postName) => {
+        setFilteredPosts(posts.filter( postData => postData.post === postName ));
     }
 
     // Component
@@ -79,9 +92,18 @@ const PostManagement = () => {
                 <div className="columna">
                     <Title>Posts</Title>
                     <div className="options-buttons">
-                        <Button klass='button' onClick={() =>{}}>Filtrar</Button>
+                        <Button klass='button' onClick={() => setFilteredPosts(posts)}>Restaurar</Button>
+                        <Button klass='button' onClick={() => applyFilter(filterValue)}>Filtrar</Button>
+                        <Input
+                            label=""
+                            id="filter-input"
+                            name="post"
+                            value={filterValue} 
+                            onChange={(e) => setFilterValue(e.target.value)}
+                            placeholder="Escribir aquí el título del post"
+                        />
                     </div>
-                    <Datatable data={posts} deleteFunction={deletePost}/>
+                    <Datatable data={filteredPost} deleteFunction={deletePost}/>
                 </div>
                 <div className="columna">
                     <Title>Crea un post</Title>
